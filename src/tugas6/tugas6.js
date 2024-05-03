d27b386327b1208ae02e0f0a0842832bd04b1f0c
nblrbbni <muhammadrabbani463@smk.belajar.id>
2024-05-03 15:22:59 +0700

    tugas-reactjs

---
+++ src/tugas6/tugas6.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Tugas6() {
+  return (
+    <div className="Content">
+      <h1>Hi, Saya Nabil</h1>
+      <hr />
+      <p>Saya dari Kelas 12 sudah berhasil menginstall React</p>
+    </div>
+  );
+}
+
+export default Tugas6;
---
+++ src/tugas7/tugas7.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Tugas7() {
+  return (
+    <div className="Content">
+      <h1>Data diri peserta kelas Reactjs</h1>
+      <hr />
+      <p>Saya dari Kelas 12 sudah berhasil menginstall React</p>
+    </div>
+  );
+}
+
+export default Tugas7;
