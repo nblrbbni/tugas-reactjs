44d1fbe85abc3ef7344cbaaa1a6dff9ab353bc84
nblrbbni <muhammadrabbani463@smk.belajar.id>
2024-05-03 15:28:17 +0700

    tugas6 & tugas7

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import Tugas6 from './tugas6/tugas6';
+import Tugas7 from './tugas7/tugas7';
 
 const App = () => {
   return (
--- src/tugas7/tugas7.js
+++ src/tugas7/tugas7.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-function Tugas7() {
+function Tugas7(props) {
   return (
     <div className="Content">
       <h1>Data diri peserta kelas Reactjs</h1>
       <hr />
-      <p>Saya dari Kelas 12 sudah berhasil menginstall React</p>
+      <ul>
+        <li>Nama: {props.name}</li>
+        <li>Kelas: {props.kelas}</li>
+        <li>Email: {props.email}</li>
+      </ul>
     </div>
   );
 }
